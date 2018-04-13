f19d9e95d63f173d6dbcb9b334e470d4520936a9
Martijn Steenbergen <m.uses+git@outlook.com>
2018-04-13 22:08:37 +0200

    Move to mono.options

--- ghdeploy/Program.cs
+++ ghdeploy/Program.cs
@@ -7,111 +7,129 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using ApiLibs.GitHub;
-using CommandLine;
+using Mono.Options;
 using Newtonsoft.Json;
 
 namespace GitHubDeployment
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            Parser.Default.ParseArguments<CommandLineOptions>(args).MapResult(
-                    options =>
+            string RepositoryName = "";
+            string Version = "";
+            string Branch = "";
+            bool NotApply = false;
+            bool Install = false;
+            bool Init = false;
+            bool ShowHelp = false;
+
+            // thses are the available options, not that they set the variables
+            var options = new OptionSet {
+                { "v|version=", "Which version to get", v => Version = v },
+                { "b|branch=", "Switch to new branch", b => Branch = b },
+                { "no-apply", "Do not immediately apply the update", n => NotApply = n != null },
+                { "i|install", "Only runs the install script", i => Install = i != null },
+                { "init", "Initialised github project in the form of user/repo", i => Init = i != null },
+                { "h|help", "show this message and exit", h => ShowHelp = h != null },
+            };
+
+            List<string> extra;
+            try
+            {
+                // parse the command line
+                extra = options.Parse(args);
+                RepositoryName = string.Join(' ', extra);
+            }
+            catch (OptionException e)
+            {
+                // output some error message
+                Console.Write("deploy: ");
+                Console.WriteLine(e.Message);
+                Console.WriteLine("Try `deploy --help' for more information.");
+                return;
+            }
+
+            if (ShowHelp)
+            {
+                Program.ShowHelp(options);
+                return;
+            }
+
+            Console.WriteLine("Starting GitHubDeployment");
+            try
+            {
+                if (Init)
+                {
+                    FirstRun(RepositoryName, Version);
+                    return;
+                }
+
+                if (Equals(RepositoryName, null))
+                {
+                    Directories.GetApplicationPath = Environment.CurrentDirectory;
+
+                    if (!File.Exists(Directories.GetPackageLocation))
                     {
+                        Console.WriteLine("You did not supply an owner/repository and you are not in a folder which has a package.json");
+                        throw new Exception();
+                    }
 
-                        int returnCode = 0;
+                }
+                else
+                {
+                    Directories.Repository = RepositoryName;
 
-                        Task.Run(async () =>
-                        {
-                            Console.WriteLine("Starting GitHubDeployment");
-                            try
-                            {
-                                if (options.Init)
-                                {
-                                    FirstRun(options);
-                                    return;
-                                }
-
-                                if (Equals(options.RepositoryName, null))
-                                {
-                                    Directories.GetApplicationPath = Environment.CurrentDirectory;
-
-                                    if (!File.Exists(Directories.GetPackageLocation))
-                                    {
-                                        Console.WriteLine("You did not supply an owner/repository and you are not in a folder which has a package.json");
-                                        returnCode = 1;
-                                        return;
-                                    }
-
-                                }
-                                else
-                                {
-                                    Directories.Repository = options.RepositoryName;
-
-                                    if (!File.Exists(Directories.GetPackageLocation))
-                                    {
-                                        Console.WriteLine("We could not find the repository you are looking for");
-                                        returnCode = 1;
-                                        return;
-                                    }
-                                }
-
-                                Package package = JsonConvert.DeserializeObject<Package>(File.ReadAllText(Directories.GetPackageLocation));
-
-                                Updater updater = new Updater(options.RepositoryName, options.Version, package);
-
-                                if (options.Install)
-                                {
-                                    updater.Install();
-                                    return;
-                                }
-
-                                updater.Fetch();
-                                bool needsInstall = false;
-
-                                if (!Equals(options.Branch, null))
-                                {
-                                    updater.ExecuteCommandLine("git", " checkout " + options.Branch);
-                                    updater.ExecuteCommandLine("git", "submodule update --recursive");
-
-                                    needsInstall = true;
-                                }
-
-                                needsInstall = needsInstall || await updater.DownloadUpdate();
-
-                                if (!options.NotApply && needsInstall)
-                                {
-                                    updater.Install();
-                                }
-                            }
-                            catch (Exception e)
-                            {
-                                Console.WriteLine(e.Message + "\n" + e.StackTrace);
-                            }
-
-                            Console.WriteLine("Exiting GitHubDeployment");
-
-                        }).Wait();
-                        
-                        return returnCode;
-                    },
-                    errors =>
+                    if (!File.Exists(Directories.GetPackageLocation))
                     {
-                        //LogHelper.Log(errors);
-                        return 1;
-                    });
+                        Console.WriteLine("We could not find the repository you are looking for");
+                        throw new Exception();
+                    }
+                }
 
-            
-             
+                Package package = JsonConvert.DeserializeObject<Package>(File.ReadAllText(Directories.GetPackageLocation));
+
+                Updater updater = new Updater(RepositoryName, Version, package);
+
+                if (Install)
+                {
+                    updater.Install();
+                    return;
+                }
+
+                updater.Fetch();
+                bool needsInstall = false;
+
+                if (!Equals(Branch, null))
+                {
+                    updater.ExecuteCommandLine("git", " checkout " + Branch);
+                    updater.ExecuteCommandLine("git", "submodule update --recursive");
+
+                    needsInstall = true;
+                }
+
+                needsInstall = needsInstall || await updater.DownloadUpdate();
+
+                if (!NotApply && needsInstall)
+                {
+                    updater.Install();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message + "\n" + e.StackTrace);
+            }
+
+            Console.WriteLine("Exiting GitHubDeployment");
+
         }
 
-        private static void FirstRun(CommandLineOptions options)
+        private static void FirstRun(string repositoryName, string version)
         {
             var package = new Package();
 
 
-            var match = Regex.Match(options.RepositoryName, "(.+)/(.+)");
+            var match = Regex.Match(repositoryName, "(.+)/(.+)");
 
             Directories.Repository = match.Groups[2].Value;
             package.Username = match.Groups[1].Value;
@@ -119,10 +137,18 @@ namespace GitHubDeployment
             Directory.CreateDirectory(Directories.GetApplicationPath);
             Directory.CreateDirectory(Directories.GetApplicationBinPath);
 
-            Updater updater = new Updater(Directories.Repository, options.Version, package);
+            Updater updater = new Updater(Directories.Repository, version, package);
 
             updater.FirstDownload();
             package.WriteToFile();
+        }
+
+        private static void ShowHelp(OptionSet options)
+        {
+            Console.WriteLine("deploy: Deploying all your work easily");
+            Console.WriteLine();
+            Console.WriteLine("Options:");
+            options.WriteOptionDescriptions(Console.Out);
         }
     }
 }
